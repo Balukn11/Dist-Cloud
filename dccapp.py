0fce5b1fbe438a890c00c35b1e5ac7a1c5c5b3da
Balukn11 <79413777+Balukn11@users.noreply.github.com>
2022-07-04 12:42:36 +0530

    Update dccapp.py

--- dccapp.py
+++ dccapp.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 
 def home():
-        return render_template("dcc2.html")
+        return render_template(["dcc2.html","dcc.css"])
 @app.route("/predict", methods=['POST'])
 def upload():
     if request.method == 'POST':
